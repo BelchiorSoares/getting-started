5b46b1be3efcedf462078af11b18ce970d5c84d4
Belchior Soares <belchiorqs@gmail.com>
2025-05-21 17:39:53 +0100

    atualizando o nome da pasta

---
+++ python/basics/4.variavel.py
@@ -0,0 +1,22 @@
+"""
+    Crie um progama para  praticar o uso de variaveis
+    com entrada e saida de dados. Declare 3 variaveis 
+    idade = 0, altura= 0.0 e  nome = ""
+
+    peca para  que o usuario informe a idade, altur e 
+    nome de uma pessoa, e mantendo seus tipos de dados
+    conforme estabelecidos  durante a declaracao delas .
+
+    Ao fnl os  dados informados na tela.
+"""
+nome = None
+altura = 0.0
+idade = 0
+
+nome = input("Nome: ")
+altura = float(input("Alturaa: "))
+idade = int(input("Idade: "))
+
+print(nome)
+print(idade)
+print(altura)
