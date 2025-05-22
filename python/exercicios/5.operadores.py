22f9f99709650ed2ddf17d38f23770fda18a34c2
Belchior Soares <belchiorqs@gmail.com>
2025-05-22 05:30:34 +0100

    Resolucao do exercicio sobre operadores

---
+++ python/exercicios/5.operadores.py
@@ -0,0 +1,13 @@
+"""
+    Cri um programa que peca ao usuario que informe  1 numero(inteiro).
+    imprima o numero infrmado, depois incremente uma undade e imprima,  
+    depois de cremente uma unidade e imprima
+"""
+
+num = int(input("Informe o numero: "))
+print(f"O numero digitado  foi: {num}")
+
+num += 1
+print(f"Incrementado: {num}")
+num -= 1
+print(f"decrementado: {num}")
